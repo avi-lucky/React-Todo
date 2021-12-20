d79ce59156fb639262923ce4c3f693af697e0dfd
avi-lucky <[shuklaavikal7@gmail.com]>
2021-12-20 23:36:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import "./App.css";
+import TextField from "@material-ui/core/TextField";
+import { useState } from "react";
+import { Button } from "@material-ui/core";
+import { db } from "./firebase_config";
+import firebase from "firebase";
+
+function App() {
+  const [todoInput, setTodoInput] = useState("");
+
+  function addTodo(e) {
+    e.preventDefault();
+
+    db.collection("todos").add({
+      inProgress: true,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      todo: todoInput,
+    });
+
+    setTodoInput("");
+  }
+
+  return (
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div>
+        <h1>React Todo</h1>
+        <form>
+          <TextField
+            id="standard-basic"
+            label="Write a Todo"
+            value={todoInput}
+            style={{ maxWidth: "300px", width: "90vw" }}
+            onChange={(e) => setTodoInput(e.target.value)}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            onClick={addTodo}
+            style={{ display: "none" }}
+          >
+            Default
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
